c6f80e3e07f07d64a4cf416db177a704584759b0
center-net <36754485+center-net@users.noreply.github.com>
2022-12-27 02:54:15 +0200

    first commit

---
+++ database/migrations/2019_02_27_224902_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->unsigned();
+            $table->double('total_price', 8, 2)->nullable();
+            $table->double('paid', 8, 2)->nullable();
+            $table->double('remaining', 8, 2)->nullable();
+            $table->timestamps();
+            
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
